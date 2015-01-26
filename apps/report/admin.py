46d35f0eea2b5e928d77584e6e8528dc40a09d93
ZUNbado <git@acs.li>
2015-01-27 00:47:13 +0100

    pdf testing

--- apps/report/admin.py
+++ apps/report/admin.py
@@ -3,9 +3,18 @@ from django.conf.urls import patterns
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response, get_object_or_404, redirect
 from django.template import RequestContext
+from django.template.loader import render_to_string
+from django.template import RequestContext
 from django.core.urlresolvers import reverse
 from datetime import datetime
 from nested_inline.admin import NestedModelAdmin, NestedStackedInline, NestedTabularInline
+
+from django.http import HttpResponse
+import ho.pisa as pisa
+import cStringIO as StringIO
+import cgi
+import os
+
 
 
 from .models import Report, ReportSection, ReportValue
@@ -42,8 +51,13 @@ class ReportAdmin(NestedModelAdmin):
     preview_template.allow_tags = True
     preview_template.short_description = 'Preview'
 
+    def pdf_template(self, obj):
+        return '<a href="pdf/%i/">Preview</a>' % obj.id
+    pdf_template.allow_tags = True
+    pdf_template.short_description = 'PDF'
+
     inlines = [ ReportSectionInline, ]
-    list_display = [ 'name', 'checklist', 'status', 'preview_template' ]
+    list_display = [ 'name', 'checklist', 'status', 'preview_template', 'pdf_template' ]
     fieldsets = (
             (None, {
                 'fields' : [ 'name', 'checklist'  ]
@@ -75,7 +89,8 @@ class ReportAdmin(NestedModelAdmin):
         urls = super(ReportAdmin, self).get_urls()
         my_urls = patterns('',
                 (r'^preview/(?P<report_id>\d+)/send/$', self.send_report),
-                (r'^preview/(?P<report_id>\d+)/$', self.preview)
+                (r'^preview/(?P<report_id>\d+)/$', self.preview),
+                (r'^pdf/(?P<report_id>\d+)/$', self.render_pdf)
                 )
         return my_urls + urls
 
@@ -88,7 +103,7 @@ class ReportAdmin(NestedModelAdmin):
 
         return redirect(reverse('admin:report_report_changelist'))
 
-    def preview(self, request, report_id):
+    def get_report_data(self, request, report_id):
         report = get_object_or_404(Report, pk=report_id)
         template = Template.objects.get(checklist=report.checklist)
         contentreplaced = []
@@ -100,11 +115,22 @@ class ReportAdmin(NestedModelAdmin):
             block.content = block.content.split('\n')
             contentreplaced.append(block)
 
+        return { 'report' : report, 'blocks' : contentreplaced }
 
-        return render_to_response('report/preview.html', {
-            'report' : report,
-            'blocks' : contentreplaced,
-            }, context_instance=RequestContext(request))
+
+    def preview(self, request, report_id):
+        return render_to_response('report/preview.html', self.get_report_data(request, report_id), context_instance=RequestContext(request))
+
+    def render_pdf(self, request, report_id):
+        data = self.get_report_data(request, report_id)
+        data['pagesize'] = 'A4'
+        html = render_to_string('report/pdf.html', data)
+        result = StringIO.StringIO()
+        pdf = pisa.pisaDocument(StringIO.StringIO(html.encode("UTF-8")), dest=result, link_callback=fetch_resources )
+        pdf.error=None
+        if not pdf.error:
+            return HttpResponse(result.getvalue(), content_type='application/pdf')
+        return HttpResponse('Gremlins ate your pdf! %s' % cgi.escape(html))
 
 
     def get_readonly_fields(self, request, obj=None):
@@ -112,6 +138,11 @@ class ReportAdmin(NestedModelAdmin):
             return [ 'checklist' ]
         return []
 
+def fetch_resources(uri ):
+        #path = os.path.join(settings.MEDIA_ROOT, uri.replace(settings.MEDIA_URL, ""))
+        path = os.path.join(settings.STATIC_ROOT, uri)
+        print "test %s " %path
+        return path
 
 
 admin.site.register(Report, ReportAdmin)
